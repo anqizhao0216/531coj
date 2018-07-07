901a1e84ae61a733671c4135f743e7c0fbb19f89
anqi <anqizhao0216@gmail.com>
2018-07-07 02:08:57 -0700

    week4 docker

---
+++ week4/oj-client/src/app/services/auth-guard.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate{
+
+  constructor(private auth: AuthService,
+              private router: Router) { }
+
+  canActivate(): boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      console.log('you need to log in first');
+      return false;
+    }
+  }
+
+//   isAdmin(): boolean {
+//     if (this.auth.isAuthenticated() && this.auth.getProfile().roles.includes('admin')) {
+//       return true;
+//     }
+//     return false;
+//   }
+}
